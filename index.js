f2c9a90dd800a1608934790d65ca05ad8ac7e2a8
Ming Li <ftntming@gmail.com>
2021-06-08 10:54:17 -0700

    Add websocket protocol to the chrome plugin

--- index.js
+++ index.js
@@ -1,128 +1,130 @@
-new function() {
-	var ws = null;
-	var connected = false;
+new function () {
+    var ws = null;
+    var connected = false;
 
-	var serverUrl;
-	var connectionStatus;
-	var sendMessage;
-	
-	var connectButton;
-	var disconnectButton; 
-	var sendButton;
+    var serverUrl;
+    var serverProtocol;
+    var connectionStatus;
+    var sendMessage;
 
-	var open = function() {
-		var url = serverUrl.val();
-		ws = new WebSocket(url);
-		ws.onopen = onOpen;
-		ws.onclose = onClose;
-		ws.onmessage = onMessage;
-		ws.onerror = onError;
+    var connectButton;
+    var disconnectButton;
+    var sendButton;
 
-		connectionStatus.text('OPENING ...');
-		serverUrl.attr('disabled', 'disabled');
-		connectButton.hide();
-		disconnectButton.show();
-	}
-	
-	var close = function() {
-		if (ws) {
-			console.log('CLOSING ...');
-			ws.close();
-		}
-		connected = false;
-		connectionStatus.text('CLOSED');
+    var open = function () {
+        var url = serverUrl.val();
+        ws = new WebSocket(url, serverProtocol.val() || "");
+        ws.onopen = onOpen;
+        ws.onclose = onClose;
+        ws.onmessage = onMessage;
+        ws.onerror = onError;
 
-		serverUrl.removeAttr('disabled');
-		connectButton.show();
-		disconnectButton.hide();
-		sendMessage.attr('disabled', 'disabled');
-		sendButton.attr('disabled', 'disabled');
-	}
-	
-	var clearLog = function() {
-		$('#messages').html('');
-	}
-	
-	var onOpen = function() {
-		console.log('OPENED: ' + serverUrl.val());
-		connected = true;
-		connectionStatus.text('OPENED');
-		sendMessage.removeAttr('disabled');
-		sendButton.removeAttr('disabled');
-	};
-	
-	var onClose = function() {
-		console.log('CLOSED: ' + serverUrl.val());
-		ws = null;
-	};
-	
-	var onMessage = function(event) {
-		var data = event.data;
-		addMessage(data);
-	};
-	
-	var onError = function(event) {
-		alert(event.data);
-	}
-	
-	var addMessage = function(data, type) {
-		var msg = $('<pre>').text(data);
-		if (type === 'SENT') {
-			msg.addClass('sent');
-		}
-		var messages = $('#messages');
-		messages.append(msg);
-		
-		var msgBox = messages.get(0);
-		while (msgBox.childNodes.length > 1000) {
-			msgBox.removeChild(msgBox.firstChild);
-		}
-		msgBox.scrollTop = msgBox.scrollHeight;
-	}
+        connectionStatus.text('OPENING ...');
+        serverUrl.attr('disabled', 'disabled');
+        connectButton.hide();
+        disconnectButton.show();
+    }
 
-	WebSocketClient = {
-		init: function() {
-			serverUrl = $('#serverUrl');
-			connectionStatus = $('#connectionStatus');
-			sendMessage = $('#sendMessage');
-			
-			connectButton = $('#connectButton');
-			disconnectButton = $('#disconnectButton'); 
-			sendButton = $('#sendButton');
-			
-			connectButton.click(function(e) {
-				close();
-				open();
-			});
-		
-			disconnectButton.click(function(e) {
-				close();
-			});
-			
-			sendButton.click(function(e) {
-				var msg = $('#sendMessage').val();
-				addMessage(msg, 'SENT');
-				ws.send(msg);
-			});
-			
-			$('#clearMessage').click(function(e) {
-				clearLog();
-			});
-			
-			var isCtrl;
-			sendMessage.keyup(function (e) {
-				if(e.which == 17) isCtrl=false;
-			}).keydown(function (e) {
-				if(e.which == 17) isCtrl=true;
-				if(e.which == 13 && isCtrl == true) {
-					sendButton.click();
-					return false;
-				}
-			});
-		}
-	};
+    var close = function () {
+        if (ws) {
+            console.log('CLOSING ...');
+            ws.close();
+        }
+        connected = false;
+        connectionStatus.text('CLOSED');
+
+        serverUrl.removeAttr('disabled');
+        connectButton.show();
+        disconnectButton.hide();
+        sendMessage.attr('disabled', 'disabled');
+        sendButton.attr('disabled', 'disabled');
+    }
+
+    var clearLog = function () {
+        $('#messages').html('');
+    }
+
+    var onOpen = function () {
+        console.log('OPENED: ' + serverUrl.val());
+        connected = true;
+        connectionStatus.text('OPENED');
+        sendMessage.removeAttr('disabled');
+        sendButton.removeAttr('disabled');
+    };
+
+    var onClose = function () {
+        console.log('CLOSED: ' + serverUrl.val());
+        ws = null;
+    };
+
+    var onMessage = function (event) {
+        var data = event.data;
+        addMessage(data);
+    };
+
+    var onError = function (event) {
+        alert(event.data);
+    }
+
+    var addMessage = function (data, type) {
+        var msg = $('<pre>').text(data);
+        if (type === 'SENT') {
+            msg.addClass('sent');
+        }
+        var messages = $('#messages');
+        messages.append(msg);
+
+        var msgBox = messages.get(0);
+        while (msgBox.childNodes.length > 1000) {
+            msgBox.removeChild(msgBox.firstChild);
+        }
+        msgBox.scrollTop = msgBox.scrollHeight;
+    }
+
+    WebSocketClient = {
+        init: function () {
+            serverUrl = $('#serverUrl');
+            serverProtocol = $('#serverProtocol');
+            connectionStatus = $('#connectionStatus');
+            sendMessage = $('#sendMessage');
+
+            connectButton = $('#connectButton');
+            disconnectButton = $('#disconnectButton');
+            sendButton = $('#sendButton');
+
+            connectButton.click(function (e) {
+                close();
+                open();
+            });
+
+            disconnectButton.click(function (e) {
+                close();
+            });
+
+            sendButton.click(function (e) {
+                var msg = $('#sendMessage').val();
+                addMessage(msg, 'SENT');
+                ws.send(msg);
+            });
+
+            $('#clearMessage').click(function (e) {
+                clearLog();
+            });
+
+            var isCtrl;
+            sendMessage.keyup(function (e) {
+                if (e.which == 17) isCtrl = false;
+            }).keydown(function (e) {
+                if (e.which == 17) isCtrl = true;
+                if (e.which == 13 && isCtrl == true) {
+                    sendButton.click();
+                    return false;
+                }
+            });
+        }
+    };
 }
 
-$(function() {
-	WebSocketClient.init();
+$(function () {
+    WebSocketClient.init();
 });
